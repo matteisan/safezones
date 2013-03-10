3de30e4001c5a456c986f4f798b054a0c6da98b8
Matt Eisan <mattjeisan@gmail.com>
2013-03-10 18:23:15 -0400

    Fixed ACF Compat.
    Need to return a table.

--- lua/acf_compatibility.lua
+++ lua/acf_compatibility.lua
@@ -22,63 +22,63 @@ end
 if ACF then 
 	local old_ACF_HE = ACF_HE 
 	function ACF_HE( Hitpos, HitNormal, FillerMass, FragMass, Inflictor, NoOcc )
-		if Hitpos:InSafeZone() then return end 
+		if Hitpos:InSafeZone() then return {} end 
 
 		old_ACF_HE( Hitpos, HitNormal, FillerMass, FragMass, Inflictor, NoOcc )
 	end 
 
 	local old_ACF_Spall = ACF_Spall 
 	function ACF_Spall( HitPos , HitVec , HitMask , KE , Caliber , Armour , Inflictor )
-		if Hitpos:InSafeZone() then return end  
+		if Hitpos:InSafeZone() then return {} end  
 
 		old_ACF_Spall( HitPos , HitVec , HitMask , KE , Caliber , Armour , Inflictor )
 	end 
 
 	local old_ACF_SpallTrace = ACF_SpallTrace 
 	function ACF_SpallTrace( HitVec , SpallTr , SpallEnergy , SpallAera , Inflictor )
-		if HitVec:InSafeZone() then return end 
+		if HitVec:InSafeZone() then return {} end 
 
 		old_ACF_SpallTrace( HitVec , SpallTr , SpallEnergy , SpallAera , Inflictor )
 	end 
 
 	local old_ACF_RoundImpact = ACF_RoundImpact 
 	function ACF_RoundImpact( Bullet, Speed, Energy, Target, HitPos, HitNormal , Bone )
-		if Hitpos:InSafeZone() then return end 
+		if Hitpos:InSafeZone() then return {} end 
 
 		old_ACF_RoundImpact( Bullet, Speed, Energy, Target, HitPos, HitNormal , Bone )
 	end 
 
 	local old_ACF_PenetrateGround = ACF_PenetrateGround 
 	function ACF_PenetrateGround( Bullet, Energy, HitPos )
-		if HitPos:InSafeZone() then return end 
+		if HitPos:InSafeZone() then return {} end 
 
 		old_ACF_PenetrateGround( Bullet, Energy, HitPos )
 	end 
 
 	local old_ACF_KEShove = ACF_KEShove 
 	function ACF_KEShove(Target, Pos, Vec, KE ) 
-		if Pos:InSafeZone() then return end 
+		if Pos:InSafeZone() then return {} end 
 
 		old_ACF_KEShove(Target, Pos, Vec, KE )
 	end 
 
 	local old_ACF_HEKill = ACF_HEKill 
 	function ACF_HEKill( Entity, HitVector, Energy )
-		if HitVector:InSafeZone() then return end 
+		if HitVector:InSafeZone() then return {} end 
 
 		old_ACF_HEKill( Entity, HitVector, Energy )
 	end 
 
 	local old_ACF_APKill = ACF_APKill 
 	function ACF_APKill( Entity , HitVector , Power )
-		if HitVector:InSafeZone() then return end 
+		if HitVector:InSafeZone() then return {} end 
 
 		old_ACF_APKill( Entity , HitVector , Power )
 	end 
 
 	local old_ACF_AmmoExplosion = ACF_AmmoExplosion 
 	function ACF_AmmoExplosion( Origin , Pos )
-		if Pos:InSafeZone() then return end 
+		if Pos:InSafeZone() then return {} end 
 
 		old_ACF_AmmoExplosion( Origin , Pos )
 	end 
