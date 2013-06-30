900f8c6ea150151d3589055c34f17902cfbd453e
Matt Eisan <mattjeisan@gmail.com>
2013-06-29 23:19:54 -0400

    e2 fixup

--- lua/entities/gmod_wire_expression2/core/custom/safezones.lua
+++ lua/entities/gmod_wire_expression2/core/custom/safezones.lua
@@ -1,8 +1,7 @@
-__e2setcost(2)
-
+E2Lib.RegisterExtension( "safezones", true )
+__e2setcost(1)
 e2function number entity:inSafeZone() 
 	if not E2Lib.IsValid( this ) then return 0 end 
 	return ( this:InSafeZone() and 1 ) or 0 
 end 
-
 __e2setcost(nil)
