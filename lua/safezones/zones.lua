04da594221ffbf7b6d7091682dd55b08cc119c84
Matt Eisan <mattjeisan@gmail.com>
2014-04-12 23:15:08 -0400

    Update zones.lua
    Changed draw to use DrawOutlinedRect to be easier on the eyes. Note: mild inaccuracies will still happen due to drawing need integers.

--- lua/safezones/zones.lua
+++ lua/safezones/zones.lua
@@ -135,59 +135,59 @@ function ZONE:Draw()
 	local max = self._max 
 	local clr = self._clr 
 	local r, g, b = clr.r, clr.g, clr.b
-	
+
 	cam.Start3D2D( max, Angle(0,0,0), 1 ) 
-		surface.SetDrawColor( Color(r, g, b, 25) )
-		
+		surface.SetDrawColor( Color(r, g, b, 255) )
+
 		local x = ( b_tonumber(max.x < corners[4].x) * max:Distance(corners[4]) )
 		local y = ( b_tonumber(max.y > corners[3].y) * max:Distance(corners[3]) )
-		
-		surface.DrawRect( 0, 0, x, y )
+
+		surface.DrawOutlinedRect( 0, 0, x, y )
 	cam.End3D2D()
-	
+
 	cam.Start3D2D( max, Angle(0,0,90), 1 )
-		surface.SetDrawColor(  Color(r, g, b, 45) )
-		
+		surface.SetDrawColor(  Color(r, g, b, 255) )
+
 		local x = ( b_tonumber(max.x < corners[4].x) * max:Distance(corners[4]) )
 		local y = ( b_tonumber(max.z > corners[2].z) * max:Distance(corners[2]) )
-		
-		surface.DrawRect( 0, 0, x, y )
+
+		surface.DrawOutlinedRect( 0, 0, x, y )
 	cam.End3D2D()
-	
+
 	cam.Start3D2D( max, Angle(90,0,0), 1 )
-		surface.SetDrawColor( Color(r, g, b, 65) )
-		
+		surface.SetDrawColor( Color(r, g, b, 255) )
+
 		local x = ( b_tonumber(max.z > corners[2].z) * max:Distance(corners[2]) )
 		local y = ( b_tonumber(max.y > corners[3].y) * max:Distance(corners[3]) )
-		
-		surface.DrawRect( 0, 0, x, y )
+
+		surface.DrawOutlinedRect( 0, 0, x, y )
 	cam.End3D2D()
-	
+
 	cam.Start3D2D( min, Angle(0,0,0), 1 ) 
-		surface.SetDrawColor( Color(r, g, b, 25) )
-		
+		surface.SetDrawColor( Color(r, g, b, 255) )
+
 		local x = ( b_tonumber(min.x < corners[7].x) * min:Distance(corners[7]) )
 		local y = ( b_tonumber(min.y > corners[6].y) * min:Distance(corners[6]) )
-		
-		surface.DrawRect( 0, 0, x, y )
+
+		surface.DrawOutlinedRect( 0, 0, x, y )
 	cam.End3D2D()
-	
+
 	cam.Start3D2D( min, Angle(0,0,90), 1 ) 
-		surface.SetDrawColor( Color(r, g, b, 45) )
-		
+		surface.SetDrawColor( Color(r, g, b, 255) )
+
 		local x = ( b_tonumber(min.x < corners[7].x) * min:Distance(corners[7]) )
 		local y = ( b_tonumber(min.z > corners[5].z) * min:Distance(corners[5]) )
-		
-		surface.DrawRect( 0, 0, x, y )
+
+		surface.DrawOutlinedRect( 0, 0, x, y )
 	cam.End3D2D()
-	
+
 	cam.Start3D2D( min, Angle(90,0,0), 1 ) 
-		surface.SetDrawColor( Color(r, g, b, 65) )
-		
+		surface.SetDrawColor( Color(r, g, b, 255) )
+
 		local x = ( b_tonumber(min.z > corners[5].z) * min:Distance(corners[5]) )
 		local y = ( b_tonumber(min.y > corners[6].y) * min:Distance(corners[6]) )
-		
-		surface.DrawRect( 0, 0, x, y )
+
+		surface.DrawOutlinedRect( 0, 0, x, y )
 	cam.End3D2D()
 end 
 
